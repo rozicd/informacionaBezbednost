37959e46c1d724b7a7cd9cccb53bdc549c0716ba
Dušan Rožić <96205750+rozicd@users.noreply.github.com>
2023-05-06 21:14:09 +0200

    feature: added paginated view of certificates on front

--- client/src/services/certificateService.js
+++ client/src/services/certificateService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:8000/api/certificate';
 
-async function getCertificates() {
-    const response = await axios.get(`${API_BASE_URL}`, {
+async function getCertificates(page) {
+    const response = await axios.get(`${API_BASE_URL}?page=${page}`, {
       withCredentials: true
     });
     return response.data;
